93eb3ba1b76a4dac2052751983425b2847d27aeb
José Roberto <tekinforroberto@gmail.com>
2025-02-09 17:16:05 -0300

    feat: Config aplicação

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,7 +6,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'JRinvest';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
